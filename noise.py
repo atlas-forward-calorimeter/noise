89904b89b03a68a6218c767c4f3c7bcad9aa5447
schrodingers-turtle <50765524+schrodingers-turtle@users.noreply.github.com>
2019-07-14 07:14:38 -0700

    Working calculation without preamp capacitor.

---
+++ noise.py
@@ -0,0 +1,94 @@
+"""
+Theoretical calculations of amplifier noise signals.
+
+Calculates electronic noise signal(s) for one center FCalPulse tube
+segment. *Without* shaping from digitzer front-end amp. All units are SI
+unless otherwise stated.
+
+omega is angular frequency.
+
+TODO: Add preamp capacitance and additional amp gain.
+
+Written by Anson Kost, adapted from code written by
+Prof. John Rutherfoord.
+"""
+
+import math
+import cmath
+import warnings
+
+import numpy
+from scipy import integrate
+from scipy.constants import pi, Boltzmann, zero_Celsius
+
+A = 39                  # Opamp amplification.
+T = 4e-9                # Cable time delay.
+rho = 50                # Cable characteristic impedance.
+C = 6e-12               # Detector capacitance.
+
+R = 26                  # Series noise source impedance.
+Q = 770                 # Parallel noise source impedance.
+
+omega_lims = (0, 1e10)  # omega integration limits.
+
+tau = rho * C           # Detector time constant.
+
+
+def F_squared(omega):
+    """Square magnitude of the series "transfer function"
+    (noise to amp output)."""
+    theta = omega * tau
+
+    # alpha is an angle that depends on the detector time constant
+    # (rho * C), but not the cable length.
+    alpha = math.acos((1 - theta ** 2) / (1 + theta ** 2))
+
+    return (
+            1 / (2 * (A + 1)) ** 2
+            * (
+                    A ** 2
+                    + (A + 2) ** 2
+                    - A * (A + 2) * math.cos(2 * omega * T + alpha)
+            )
+    )
+
+
+def G(omega):
+    """The parallel "transfer function." """
+    theta_i = omega * tau * 1j
+    phi_i = omega * (2 * T) * 1j
+    return 1 / 2 * (
+            1 + ((1 - theta_i) / (1 + theta_i)) * cmath.exp(-phi_i)
+    )
+
+
+def G_squared(omega):
+    """Square magnitude of the parallel "transfer function." """
+    return abs(G(omega)) ** 2
+
+
+def H_squared(omega):
+    """Square magnitude of the frequency filter function."""
+    return numpy.heaviside(omega_lims[1] - omega, 1 / 2)
+
+
+def integrand(omega):
+    """Just the factors that change over the integration."""
+    return (
+                   R * F_squared(omega)
+                   + (rho ** 2 / Q) * G_squared(omega)
+           ) * H_squared(omega)
+
+
+integration_result, error = integrate.quad(
+    integrand, omega_lims[0], omega_lims[1]
+)
+
+# A naive check on the integration error.
+if abs(error / integration_result) > 1e-7:
+    warnings.warn("The integration error is relatively big!")
+
+constant_factor = (2 / pi) * Boltzmann * zero_Celsius * A ** 2
+noise_rms_voltage = math.sqrt(constant_factor * integration_result)
+
+print(f"Noise signal RMS voltage: {round(1000 * noise_rms_voltage, 4)} mV.")
---
+++ sympynoise.py
@@ -0,0 +1,81 @@
+"""
+This is a work in progress that uses sympy to do the same calculations
+as noise.py. It's not finished!
+
+Theoretical calculations of amplifier noise signals.
+
+Calculates electronic noise signal(s) for one center FCalPulse tube
+segment. *Without* shaping from digitzer front-end amp.
+
+TODO: Add preamp capacitance.
+
+Written by Anson Kost, adapted from code written by
+Prof. John Rutherfoord.
+
+Variable names:
+    Universal constants:
+        k: Boltzmann constant.
+
+    Circuit properties:
+        A: Opamp amplification.
+
+        rho: Cable characteristic impedance.
+        T: Cable time delay.
+        C: Detector capacitance.
+        alpha: An angle that depends on the detector time constant (rho * C)
+        (but not the cable length).
+
+        R: Series noise source impedance.
+        Q: Parallel noise source impedance.
+
+        F2: Square magnitude of the series "transfer function"
+        (noise to amp output).
+        G2: Square magnitude of the parallel "transfer function."
+
+        H2: Square magnitude of the frequency filter function.
+
+    Other:
+        omega: Angular frequency.
+"""
+
+import datetime
+import sympy
+from sympy.abc import k, A, rho, R, Q, C, T, alpha, omega
+from scipy import integrate, inf
+
+omega_cutoff = 1e10
+
+print(datetime.datetime.now())
+
+theta = omega * rho * C
+alpha = sympy.acos((1 - theta ** 2) / (1 + theta ** 2))
+
+F2 = (
+        1 / (2 * (A + 1)) ** 2
+        * (
+                A ** 2
+                + (A + 2) ** 2
+                - A * (A + 2) * sympy.cos(2 * omega * T + alpha)
+        )
+)
+
+H2 = sympy.Heaviside(omega_cutoff - omega)
+
+symbolic_integrand = F2 * H2
+
+inputs = {
+        'A': 39,
+        'rho': 50,
+        'T': 4e-9,
+        'C': 6e-12
+}
+numeric_integrand = sympy.lambdify(
+        omega,
+        symbolic_integrand.subs(inputs),
+        modules=('numpy', 'sympy')
+)
+noise_V2 = integrate.quad(numeric_integrand, 0, omega_cutoff)
+
+print(noise_V2)
+
+print(datetime.datetime.now())
